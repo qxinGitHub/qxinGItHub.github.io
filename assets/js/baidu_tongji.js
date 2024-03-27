0712334276ef0f435efd48d5acb29449358ed935
qxinGitHub <qxguge@gmail.com>
2024-03-27 22:10:41 +0800

    增加百度统计代码

---
+++ assets/js/baidu_tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?0bf0112daf25720187316f61898ed138";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
